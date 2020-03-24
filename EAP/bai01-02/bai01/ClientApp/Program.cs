a560ef4052942d86e0c6ad03fb22f1b1176009e9
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-03-24 12:52:42 +0700

    update

---
+++ EAP/bai01-02/bai01/ClientApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+namespace ClientApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync("https://localhost:44392/api/EmployeesAPI");
+            string responseBody = await response.Content.ReadAsStringAsync();//convert response =>string
+            //Install-Package Newtonsoft.Json -Version 12.0.3
+            var objects = JsonConvert.DeserializeObject<object>(responseBody);
+            //https://www.newtonsoft.com/json/help/html/DeserializeObject.htm
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine("Hello World!");
+            //Tham khao:
+            //https://www.c-sharpcorner.com/article/calling-web-api-using-httpclient/
+
+        }
+    }
+}
--- EAP/bai01-02/bai01/HttpClientSample/Program.cs
+++ EAP/bai01-02/bai01/HttpClientSample/Program.cs
@@ -14,102 +14,18 @@ namespace HttpClientSample
 {
     public class Program
     {
+        /*
         static HttpClient client = new HttpClient();
-        static void ShowEmployee(Employee employee)
+        static async Task GetAllEmployees()
         {
-            Console.WriteLine($"Name: {employee.EmpName}\t");
+            var result = await client.GetAsync("https://localhost:44392/api/EmployeesAPI");
+            Console.WriteLine(result.StatusCode);
         }
-        static async Task<Uri> CreateEmployeeAsync(Employee employee)
-        {
-            HttpResponseMessage response = await client.PostAsJsonAsync("api/EmployeesAPI",employee);
-            response.EnsureSuccessStatusCode();
-            // return URI of the created resource.
-            return response.Headers.Location;
-        }
-        static async Task<Employee> GetEmployeeAsync(string path)
-        {
-            Employee employee = null;
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                employee = await response.Content.ReadAsAsync<Employee>();
-            }
-            return employee;
-        }
+        */
 
-        static async Task<Employee> UpdateEmployeeAsync(Employee employee)
-        {
-            HttpResponseMessage response = await client.PutAsJsonAsync(
-                $"api/EmployeesAPI/{employee.EmpID}", employee);
-            response.EnsureSuccessStatusCode();
-
-            // Deserialize the updated employee from the response body.
-            employee = await response.Content.ReadAsAsync<Employee>();
-            return employee;
-        }
-
-        static async Task<HttpStatusCode> DeleteEmployeeAsync(int id)
-        {
-            HttpResponseMessage response = await client.DeleteAsync(
-                $"api/EmployeesAPI/{id}");
-            return response.StatusCode;
-        }
-
-        static void Main()
-        {
-            RunAsync().GetAwaiter().GetResult();
-        }
-
-        static async Task RunAsync()
-        {
-            // Update port # in the following line.
-            client.BaseAddress = new Uri("https://localhost:44392/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-
-            try
-            {
-                // Create a new employee
-                Employee employee = new Employee
-                {
-                    EmpName = "Hoangnnn",
-                    DeptID = 2,
-                    Address = "nha x ngo y",
-                    Email = "hoannn@gmail.com",
-                };
-
-                var url = await CreateEmployeeAsync(employee);
-                Console.WriteLine($"Created at {url}");
-
-                // Get the employee
-                employee = await GetEmployeeAsync(url.PathAndQuery);
-                ShowEmployee(employee);
-
-                // Update the employee
-                Console.WriteLine("Updating price...");
-                employee.DeptID = 3;
-                await UpdateEmployeeAsync(employee);
-
-                // Get the updated employee
-                employee = await GetEmployeeAsync(url.PathAndQuery);
-                ShowEmployee(employee);
-
-                // Delete the employee
-                var statusCode = await DeleteEmployeeAsync(employee.EmpID);
-                Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            Console.ReadLine();
-        }
         public static void Main(string[] args)
         {
-            RunAsync().GetAwaiter().GetResult();
+            Console.WriteLine("hello");
         }
 
     }
