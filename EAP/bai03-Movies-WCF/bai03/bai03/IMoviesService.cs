ff24972c45ad3032786be99315dd6af77731dd93
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-03-25 07:00:37 +0700

    update

---
+++ EAP/bai03-Movies-WCF/bai03/bai03/IMoviesService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace bai03
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMoviesService" in both code and config file together.
+    [ServiceContract]
+    public interface IMoviesService
+    {
+        [OperationContract]
+        double AddNumber(double x, double y);
+        /*
+         [OperationContract]
+        List<Movie> GetAll();
+
+        [OperationContract]
+        List<Movie> Search(string Title);
+        
+        [OperationContract]
+        Movie GetById(int Id);
+        
+        [OperationContract]
+        void Add(Movie m);
+        
+        [OperationContract]
+        void Edit(Movie m);
+        [OperationContract]
+        void Delete(int Id);
+         */
+    }
+}
