c53a731b8b5703f0880bbc1c2d92d631f97d0560
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-03-31 10:01:08 +0700

    update

---
+++ EAP/bai03-wcfService/WCFConsoleApp/MovieManagement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFConsoleApp.MovieServiceReference;
+
+namespace WCFConsoleApp
+{
+    class MovieManagement
+    {
+        public void getAll()
+        {
+            MovieServiceClient client = new MovieServiceClient();
+            Movie[] movies = client.getAll();
+            foreach(Movie movie in movies)
+            {
+                Console.WriteLine("Title = " + movie.Title);
+                Console.WriteLine("MovieId = " + movie.MovieId);
+                Console.WriteLine("ReleaseDate = " + movie.ReleaseDate);
+                Console.WriteLine("RunningTime = " + movie.RunningTime);
+                Console.WriteLine("GenreId = " + movie.GenreId);
+            }
+            client.Close();
+        }
+    }
+}
---
+++ EAP/bai03-wcfServiceTemp/bai03-wcfService/MovieService.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace bai03_wcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MovieService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MovieService.svc or MovieService.svc.cs at the Solution Explorer and start debugging.
+    public class MovieService : IMovieService
+    {
+        public List<Movie> getAll()
+        {
+            using (MoviesEntities db = new MoviesEntities())
+            {
+
+                db.Configuration.ProxyCreationEnabled = false;
+                return db.Movies.ToList();                
+            }
+            
+        }
+        public List<Movie> Search(String text)
+        {
+            using (MoviesEntities db = new MoviesEntities())
+            {
+
+                db.Configuration.ProxyCreationEnabled = false;
+                var results = from movie in db.Movies
+                              where movie.Title.Contains(text)
+                              select movie;
+                
+                return results.ToList();                
+            }
+
+            
+        }
+    }
+}
